9f578bfffd2c5ecc0a6ad28c836ba960ec84142e
mike <mike@unottingham.ac.uk>
2026-05-29 14:54:21 +0100

    anchor

---
+++ Instructions.md
@@ -0,0 +1,2 @@
+# Add some instructions to students
+
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import numpy
+import pandas
+import scipy
+import matplotlib
+
+
+
 def main():
+    # %% 
     print("Hello from template!")
 
+    #%% 
+    import sys; print(sys.executable)
+
+    #%%
 
 if __name__ == "__main__":
     main()
