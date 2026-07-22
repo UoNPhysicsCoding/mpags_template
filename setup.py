8a7ea0169b3d5fe40027e2ee1c8d3714992cc668
mikei <mikei@unottingham.ac.uk>
2026-07-22 10:31:35 +0100

    refined instructions

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def run():
 
         print(f"Creating new venv at: {safe_path}")
         safe_path.parent.mkdir(parents=True, exist_ok=True)
-        subprocess.run(["uv", "venv", "--force", str(safe_path)], check=True)
+        subprocess.run(["uv", "venv", "--allow-existing", str(safe_path)], check=True)
 
     # 4. Set Environment Variables & Sync
     env = os.environ.copy()
@@ -67,12 +67,16 @@ def run():
         new_python_path = safe_path.joinpath(Path("bin")).joinpath(Path("python"))
         new_activate_path = safe_path.joinpath(Path("bin")).joinpath(Path("activate"))
 
-    print(f"\nEnvironment files stored at {safe_path}")
-    print("Setup complete.")
-    print(f"Select Python Interpreter: {new_python_path}")
-    print(f"Activate in terminal: {new_activate_path.joinpath(Path("activate"))}")
-    print(f"If on Mac or Linux may need to chmod +x to enable permissions for activation")
-
+    print(f"\n\n\n==========================================================\n")
+    print(f"Setup Instructions:\n")
+    print("1. Highlight and copy using ctrl + c the path to your python interpreter below:\n")
+    print(f"Select Python Interpreter: {new_python_path.as_posix()}\n")
+    print(f"2. Type code . into the terminal to open vscode")
+    print(f"3. Type `ctrl + shift + p` and type and select `Python: Select Interpreter")
+    print(f"4. Select `Enter Interpreter Path` and paste the path into the box followed by hit Enter")
+    print(f"5. When you open a Jupyter Notebook click `Detect Kernel`, `Python Environments` and then select {project_name}_env")
+    print(f"If you need these instructions again you can rerun the script. It's ok to close this window")
+    print(f"\n==========================================================\n")
 
 if __name__ == '__main__':
     run()
